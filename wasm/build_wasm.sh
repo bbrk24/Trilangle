5d87f97e0e48c8276d67ea19550326566f6854e0
William Baker <bbrk24@gmail.com>
2023-02-22 21:14:55 -0500

    Disable assertions in wasm

--- wasm/build_wasm.sh
+++ wasm/build_wasm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 emcc ../*.cpp \
-    -O2 -flto -fno-rtti -fno-exceptions \
+    -O2 -flto -fno-rtti -fno-exceptions -DNDEBUG \
     -sEXPORTED_RUNTIME_METHODS=ccall \
     -o out.js \
     --pre-js pre.js -sINCOMING_MODULE_JS_API=preInit,noExitRuntime
