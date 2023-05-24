2465416e27361ce7e92aee8d6382216fe550b3ed
William Baker <bbrk24@gmail.com>
2023-05-23 20:51:10 -0400

    Add workflows.yml

--- wasm/build_wasm.sh
+++ wasm/build_wasm.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-npx coffee -co pre.js worker.coffee
-# pre.js must be compiled before emcc may begin
-
 common_emcc_args=(-WCL4 -Wnon-gcc -Wno-nullability-completeness
     -fno-rtti -fno-exceptions
     -sEXPORTED_RUNTIME_METHODS=ccall
@@ -13,14 +10,26 @@ common_emcc_args=(-WCL4 -Wnon-gcc -Wno-nullability-completeness
 
 if [ "$1" = debug ]
 then
-    npx coffee -cMo index.js in.coffee &
-    emcc ../*.cpp "${common_emcc_args[@]}" -Wno-unused-value -Og -g2 -sASSERTIONS &
-    npx sass in.scss index.css --no-source-map &
+    # Without the subshell, the first two waits won't actually affect the exit code of the script as a whole.
+    (
+        npx coffee -cMo index.js in.coffee &
+        {
+            npx coffee -co pre.js worker.coffee
+            emcc ../*.cpp "${common_emcc_args[@]}" -Wno-unused-value -Og -g2 -sASSERTIONS
+        } &
+        npx sass in.scss index.css --no-source-map &
+        wait %1 && wait %2 && wait %3
+    )
 else
-    npx coffee -co index.js in.coffee &
-    emcc ../*.cpp "${common_emcc_args[@]}" -O3 -flto -DNDEBUG --closure 1 --closure-args='--emit_use_strict' &
-    npx sass in.scss index.css --no-source-map -s compressed &
+    (
+        npx coffee -co index.js in.coffee &
+        {
+            npx coffee -co pre.js worker.coffee
+            emcc ../*.cpp "${common_emcc_args[@]}" -O3 -flto -DNDEBUG --closure 1 --closure-args='--emit_use_strict'
+        } &
+        npx sass in.scss index.css --no-source-map -s compressed &
+        wait %1 && wait %2 && wait %3
+    )
 fi
 
-wait
 rm pre.js
