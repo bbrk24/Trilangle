6cd303eb8dbd957530fe68a3a7f2a782edafed0c
William Baker <bbrk24@gmail.com>
2023-05-24 23:58:20 -0400

    move C++ code into src/

--- wasm/build_wasm.sh
+++ wasm/build_wasm.sh
@@ -15,7 +15,7 @@ then
         npx coffee -cMo index.js in.coffee &
         {
             npx coffee -co pre.js worker.coffee
-            emcc ../*.cpp "${common_emcc_args[@]}" -Wno-unused-value -Og -g2 -sASSERTIONS
+            emcc ../src/*.cpp "${common_emcc_args[@]}" -Wno-unused-value -Og -g2 -sASSERTIONS
         } &
         npx sass in.scss index.css --no-source-map &
         wait %1 && wait %2 && wait %3
@@ -25,7 +25,7 @@ else
         npx coffee -co index.js in.coffee &
         {
             npx coffee -co pre.js worker.coffee
-            emcc ../*.cpp "${common_emcc_args[@]}" -O3 -flto -DNDEBUG --closure 1 --closure-args='--emit_use_strict'
+            emcc ../src/*.cpp "${common_emcc_args[@]}" -O3 -flto -DNDEBUG --closure 1 --closure-args='--emit_use_strict'
         } &
         npx sass in.scss index.css --no-source-map -s compressed &
         wait %1 && wait %2 && wait %3
