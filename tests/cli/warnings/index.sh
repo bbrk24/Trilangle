893fc40d534f2a9cab2578c0910f0965d0b93ccd
William Baker <bbrk24@gmail.com>
2023-07-30 00:27:04 -0400

    That -w is useless

--- tests/cli/warnings/index.sh
+++ tests/cli/warnings/index.sh
@@ -3,7 +3,7 @@
 folder="$(dirname "$0")"
 
 # Without the braces, the &> doesn't silence the floating-point exception
-{ trilangle -w "${folder}/worse.trg"; } &>/dev/null
+{ trilangle "${folder}/worse.trg"; } &>/dev/null
 result=$?
 set -e
 test $result -ne 0
