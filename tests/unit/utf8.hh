ac6ae82618f931b80da39c171d4aa9416197c68d
William Baker <bbrk24@gmail.com>
2023-07-29 22:59:14 -0400

    Oh, std::vector already has operator==

--- tests/unit/utf8.hh
+++ tests/unit/utf8.hh
@@ -5,7 +5,6 @@
 #include <sstream>
 #include <string_processing.hh>
 #include "test-framework/test_framework.hh"
-#include "vector_compare.hh"
 
 using std::ostringstream;
 using std::string;
@@ -19,21 +18,21 @@ testgroup (utf8_parsing) {
     , testcase (ascii_string) {
         vector<int24_t> parsed = parse_utf8("abcd", false);
         vector<int24_t> expected = { 'a', 'b', 'c', 'd' };
-        test_assert(vector_equal(parsed, expected), "ASCII characters should maintain their values");
+        test_assert(parsed == expected, "ASCII characters should maintain their values");
     }
     , testcase (shebangs) {
         vector<int24_t> parsed1 = parse_utf8("#!/bin/trilangle\nabc", true);
         vector<int24_t> expected1 = { 'a', 'b', 'c' };
-        test_assert(vector_equal(parsed1, expected1), "Shebang should be removed");
+        test_assert(parsed1 == expected1, "Shebang should be removed");
 
         vector<int24_t> parsed2 = parse_utf8("#!@", false);
         vector<int24_t> expected2 = { '#', '!', '@' };
-        test_assert(vector_equal(parsed2, expected2), "Shebang should be kept");
+        test_assert(parsed2 == expected2, "Shebang should be kept");
     }
     , testcase (multibyte_characters) {
         vector<int24_t> parsed = parse_utf8(".\xC3\xA9.e\xCC\x81.\xF0\x9F\x8E\x88", false);
         vector<int24_t> expected = { '.', INT24_C(0x00e9), '.', 'e', INT24_C(0x0301), '.', INT24_C(0x1f388) };
-        test_assert(vector_equal(parsed, expected));
+        test_assert(parsed == expected);
     }
 };
 
--- tests/unit/vector_compare.hh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <algorithm>
-#include <vector>
-
-template<typename T>
-bool vector_equal(const std::vector<T>& lhs, const std::vector<T>& rhs) noexcept {
-    return lhs.size() == rhs.size() && std::equal(lhs.begin(), lhs.end(), rhs.begin());
-}
