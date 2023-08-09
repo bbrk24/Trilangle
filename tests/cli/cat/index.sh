f37a7f063b968fd9d843683bf1524e399f3e7cf5
William Baker <bbrk24@gmail.com>
2023-08-09 00:02:40 -0400

    I love debugging CI

--- tests/cli/cat/index.sh
+++ tests/cli/cat/index.sh
@@ -12,6 +12,10 @@ text='
 folder="$(dirname "$0")"
 echo "Script location: $folder. Running script..."
 output="$(trilangle "${folder}/cat.trg" <<<"$text")"
-echo "Script ran successfully. Output: $output"
+echo "Script ran successfully. Output: $output. Expected: $text"
 
+set +e
 test "$text" = "$output"
+result=$?
+echo $result
+exit $result
