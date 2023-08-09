31eebacd6a27e86da16ef81b3d86e04e2ed697d1
William Baker <bbrk24@gmail.com>
2023-08-09 00:08:56 -0400

    Is it seriously carriage return

--- tests/cli/cat/index.sh
+++ tests/cli/cat/index.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'Beginning script...'
 
 set -e
 
@@ -10,12 +9,8 @@ text='
 (@)<_____>__(_____)____/'
 
 folder="$(dirname "$0")"
-echo "Script location: $folder. Running script..."
-output="$(trilangle "${folder}/cat.trg" <<<"$text")"
-echo "Script ran successfully. Output: $output. Expected: $text"
 
-set +e
-test "$text" = "$output"
-result=$?
-echo $result
-exit $result
+output="$(trilangle "${folder}/cat.trg" <<<"$text")"
+
+# Okay so on Windows the newline characters are different. Isn't this fun?
+test "$text" = "${output//$'\r'/}"
