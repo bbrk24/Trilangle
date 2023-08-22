cacab5d0a271e9ea3d8005e3d0639fb869b3a94d
William Baker <bbrk24@gmail.com>
2023-08-22 00:49:25 -0400

    Add lint mode to wasm

--- wasm/build_wasm.sh
+++ wasm/build_wasm.sh
@@ -12,16 +12,24 @@ npx coffee -co pre.js worker.coffee &
 npx coffee -co library.js library.coffee
 wait
 
-if [ "$1" = debug ]
-then
-    emcc ../src/*.cpp "${common_emcc_args[@]}" -Og -g2 -sASSERTIONS &
-    # emcc is so much slower than the coffeescript and sass compilers that parallelizing them doesn't matter much
-    npx coffee -cMo index.js in.coffee
-    npx sass in.scss index.css --embed-source-map
-else
-    emcc ../src/*.cpp "${common_emcc_args[@]}" -O3 -flto -DNDEBUG --closure 1 --closure-args='--emit_use_strict' &
-    npx coffee -co index.js in.coffee
-    npx sass in.scss index.css --no-source-map -s compressed
-fi
+case "$1" in
+    debug)
+        emcc ../src/*.cpp "${common_emcc_args[@]}" -Og -g2 -sASSERTIONS &
+        # emcc is so much slower than the coffeescript and sass compilers that parallelizing them doesn't matter much
+        npx coffee -cMo index.js in.coffee
+        npx sass in.scss index.css --embed-source-map
+        ;;
+    lint)
+        emcc ../src/*.cpp "${common_emcc_args[@]}" -Werror -O0 -DNDEBUG --closure 1 &
+        npx coffee -cp in.coffee >/dev/null
+        # Why does sass not give its version in a format it accepts?
+        npx sass in.scss /dev/null --no-source-map --fatal-deprecation "$(npx sass --version | cut -d' ' -f1)"
+        ;;
+    *)
+        emcc ../src/*.cpp "${common_emcc_args[@]}" -O3 -flto -DNDEBUG --closure 1 --closure-args='--emit_use_strict' &
+        npx coffee -co index.js in.coffee
+        npx sass in.scss index.css --no-source-map -s compressed
+        ;;
+esac
 
 wait
