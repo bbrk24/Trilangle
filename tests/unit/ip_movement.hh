5de3647c30c597aa1ff7b7ee50dc74343516ddbf
William Baker <bbrk24@gmail.com>
2023-09-13 09:46:27 -0400

    Add branch tests

--- tests/unit/ip_movement.hh
+++ tests/unit/ip_movement.hh
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <program_walker.hh>
+#include <tuple>
 #include "test-framework/test_framework.hh"
+
+using std::pair;
 
 namespace {
 // Thank you clang-format very cool
@@ -12,7 +15,7 @@ namespace {
 
 // Each test program consists of every digit 0-9 exactly once, in the order they are hit if the IP is traveling in the
 // given direction from the top corner.
-std::initializer_list<std::pair<const char*, std::pair<direction, program>>> test_programs = {
+std::initializer_list<pair<const char*, pair<direction, program>>> test_programs = {
     TEST_ITEM(southwest, "0142573689"),
     TEST_ITEM(west, "0215439876"),
     TEST_ITEM(northwest, "0395286417"),
@@ -22,10 +25,23 @@ std::initializer_list<std::pair<const char*, std::pair<direction, program>>> tes
 };
 
 #undef TEST_ITEM
+
+#define LR_PAIR(left, left_branch, right, right_branch) \
+    { #right "_left", { direction::right, right_branch, true, direction::left } }, { \
+#left "_right", { direction::left, left_branch, false, direction::right } \
+    }
+
+// Each item is a 4-tuple of starting direction, instruction, should go left, ending direction.
+std::initializer_list<pair<const char*, std::tuple<direction, int24_t, bool, direction>>> branch_tests = {
+    LR_PAIR(southwest, '7', west, '>'), LR_PAIR(west, '>', northwest, 'v'), LR_PAIR(northwest, 'v', northeast, 'L'),
+    LR_PAIR(northeast, 'L', east, '<'), LR_PAIR(east, '<', southeast, '^'), LR_PAIR(southeast, '^', southwest, '7'),
+};
+
+#undef LR_PAIR
 }  // namespace
 
 test_iter(ip_advance, test_programs, input) {
-    program& p = input.second;
+    const program& p = input.second;
     program_walker::instruction_pointer ip{ { SIZE_C(0), SIZE_C(0) }, input.first };
 
     for (int24_t i = '0'; i <= '9'; ++i) {
@@ -35,4 +51,10 @@ test_iter(ip_advance, test_programs, input) {
     test_assert(ip.coords.first == 0 && ip.coords.second == 0);
 };
 
-// TODO: Add tests for branch instructions
+test_iter(ip_branch, branch_tests, input) {
+    direction dir = std::get<0>(input);
+    program_walker::branch(dir, std::get<1>(input), [&]() NOEXCEPT_T { return std::get<2>(input); });
+    test_assert(dir == std::get<3>(input));
+};
+
+// TODO: add mirror tests
