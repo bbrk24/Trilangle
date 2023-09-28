241d1ee3d005cd9c186b9c3f6740f525f555fa0a
William Baker <bbrk24@gmail.com>
2023-09-28 12:26:59 -0400

    Well.

--- tests/cli/ascii/index.sh
+++ tests/cli/ascii/index.sh
@@ -5,15 +5,19 @@ set -e
 folder="$(dirname "$0")"
 
 warns1="$(trilangle -aw "${folder}/read.trg" 2>&1 <<<'é')"
+
 # Technically it is possible to avoid writing this twice. But it's so convoluted that it's easier to not.
 warns2="$(trilangle -aw "${folder}/write.trg" 2>&1 1<&-)"
-out2="$(trilangle -aw "${folder}/write.trg" 2>/dev/null)"
+# This value will vary depending on the platform. Some will normalize it to a multi-byte substitution character.
+# Similarly, using `-m` will cause an error on some platforms and report '1' on others.
+bytecount="$(trilangle -aw "${folder}/write.trg" 2>/dev/null | wc -c)"
 
 nowarn="$(trilangle -aw "${folder}/read.trg" 2>&1 <<<'e')"
 e=$(trilangle -aw "${folder}/fine.trg")
 
 test -n "$warns1"
 test -n "$warns2"
-test 1 -eq "$(wc -m <<<"$out2")"
+test 4 -ge "$bytecount"
+test 1 -le "$bytecount"
 test -z "$nowarn"
 test e = "$e"
