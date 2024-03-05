f5d73d488b4f307db54fcb7d777b6de75ea4586b
William Baker <bbrk24@gmail.com>
2024-03-05 09:10:47 -0500

    apparently this was just sitting in my stash

--- tests/cli/null/index.sh
+++ tests/cli/null/index.sh
@@ -12,10 +12,8 @@ run_with_z () {
     printf '%s\0%s' "$1" "$2" | $TRILANGLE -z
 }
 
-one=$(run_with_z '?!@' 1)
-z=$(run_with_z 'io@' z)
-output=$(run_with_z '<>i,@##o' "$text")
+test 1 = "$(run_with_z '?!@' 1)"
+test z = "$(run_with_z 'io@' z)"
 
-test 1 = "$one"
-test z = "$z"
+output=$(run_with_z '<>i,@##o' "$text")
 test "$text" = "${output//$'\r'/}"
