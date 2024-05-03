45da76f34ae5d28b94b60986c9b2d50e7518a94e
William Baker <bbrk24@gmail.com>
2024-05-03 11:04:52 -0400

    Weird

--- tests/cli/warnings/index.sh
+++ tests/cli/warnings/index.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -ux
+set -u
 
 folder=$(dirname "$0")
 
 # Without the braces, the &> doesn't silence the floating-point exception
-errors=$($TRILANGLE -w "${folder}/worse.trg" 2>&1 1<&-)
-result=$?
-set -e
-test 0 -ne $result
+{ errors=$($TRILANGLE -w "${folder}/worse.trg" 2>&1 1<&-); } &>/dev/null
+# For some reason, macOS 14 doesn't trigger a floating-point exception, so we can't verify a nonzero
+# exit code.
 
+set -e
 test -n "$errors"
 test -n "$($TRILANGLE -w "${folder}/bad.trg" 2>&1)"
